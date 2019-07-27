955f1b38b558e0fdbecd061e11d730843eecdec7
Andrey Ponomarev <info@andreyponomarev.ru>
2019-07-27 03:40:14 +0300

    Refactor according to BEM

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    'env': {
+        'browser': true,
+        'es6': true
     },
-    "extends": [ "airbnb-base", "plugin:prettier/recommended" ], // or "eslint:recommended" or  [ "airbnb-base", "prettier"] 
-    "plugins": ["prettier"],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    'extends': [ 'airbnb-base', 'plugin:prettier/recommended' ], // or "eslint:recommended" or  [ "airbnb-base", "prettier"] 
+    'plugins': [ 'prettier' ],
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
     },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
     },
-    "rules": {
-      "func-names": ["error", "never"],
+    'rules': {
+      'func-names': ['error', 'never'],
       'no-console': 'off',
-      "prettier/prettier": ["error"],
+      'prettier/prettier': ['error'],
+      'no-multiple-empty-lines': [2, {'max': 3, 'maxEOF': 0, 'maxBDF': 0}]
     },
 };
--- src/index.js
+++ src/index.js
@@ -1,32 +1,26 @@
-// css reset
+/*************
+ * CSS RESET *
+ *************/
 import './_reset.scss';
 
-// *** IMPORT BLOCKS ***
+/**********
+ * BLOCKS *
+ **********/
 
 // link
 import './blocks.common/link/_link.scss';
-// heading
-import './blocks.common/heading/_heading.scss';
-// list
-import './blocks.common/list/_list.scss';
 // section
 import './blocks.common/section/_section.scss';
 import './blocks.common/section/section.js';
+//article
+import './blocks.common/article/_article.scss';
 // paragraph
 import './blocks.common/paragraph/_paragraph.scss';
-// button
-import './blocks.common/button/_button.scss';
-// logo
-import './blocks.common/logo/_logo.scss';
-import './blocks.common/logo/logo.js';
-// icon
-import './blocks.common/icons/_icons.scss';
-// horizontal rule
-import './blocks.common/horizontal-rule/_horizontal-rule.scss';
+// image
+import './blocks.common/image/_image.scss';
+// heading
+import './blocks.common/heading/_heading.scss';
 
-// page
-import './blocks.common/page/_page.scss';
-import './blocks.common/page/page.js';
 // page-header
 import './blocks.common/page-header/_page-header.scss';
 import './blocks.common/page-header/page-header.js';
@@ -34,8 +28,14 @@ import './blocks.common/page-header/page-header.js';
 import './blocks.common/page-main/_page-main.scss';
 // page-footer
 import './blocks.common/page-footer/_page-footer.scss';
+// page
+import './blocks.common/page/_page.scss';
+import './blocks.common/page/page.js';
 
-// IMPORT GRAPHICS
+/************
+ * GRAPHICS *
+ ************/
+
 function requireAllFiles(r) {
   r.keys().forEach(r);
 }
