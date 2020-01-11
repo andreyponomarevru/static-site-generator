0b9beaa66c24f105790f3ad0c21b3385c62cf93c
Andrey Ponomarev <info@andreyponomarev.ru>
2020-01-11 13:39:44 +0300

    Add Composer

--- index.php
+++ index.php
@@ -1,26 +1,32 @@
-doctype html
-html(lang='en' class='page')
-    meta(charset='utf-8')
-    meta(http-equiv='x-ua-compatible' content='ie=edge')
-    meta(name='viewport' content='width=device-width, initial-scale=1.0, viewport-fit=cover')
-    meta(name='description' content='Description of the page less than 150 characters')
-    
-    //- Displayed in Google search results
-    meta(name='keywords' content='web development, web applications, frontend')
-    meta(name='author' content='Andrey Ponomarev')
+<!DOCTYPE html>
+<html class="page" lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover,shrink-to-fit=no">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="format-detection" content="date=no">
+    <meta name="format-detection" content="address=no">
+    <meta name="format-detection" content="email=no">
+    <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+    <link rel="manifest" href="./site.webmanifest">
+    <link rel="shortcut icon" href="./favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="./browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,700i,800,800i&amp;display=swap&amp;subset=cyrillic,cyrillic-ext,latin-ext" rel="stylesheet">
+    <meta name="description" content="Исправляется и корректируется почерк за 7 дней, ни одна ручка на это не способна">
+    <meta name="keywords" content="web development, frontend, test">
+    <meta name="author" content="Andrey Ponomarev">
+    <title>Andrey Ponomarev</title>
+  <link href="./styles.83e1ed0cdb01415efb1c.css" rel="stylesheet"></head>
+  <body class="page__body page__preload">
+    <noscript>Your browser doesn't support JavaScript or you have disabled JavaScript.</noscript>
 
-    //- 32x32px 
-    link(rel='shortcut icon' href='./img/favicon.ico' type='image/x-icon') 
 
-    link(rel='icon' href='./img/icon.png' type='image/png')
-    //- Apple Touch Icon (at least 200x200px)
-    link(rel='apple-touch-icon' href='./img/custom-icon.png')
     
-    title Andrey Ponomarev. Portfolio
-  
-    body(class='page__body page__preload')
-      
-      //- div(class='page__page-layout') - helper class (wrapper), enable if needed
-      include ../page-header/page-header.pug
-      include ../page-main/page-main.pug
-      include ../page-footer/page-footer.pug
+    <script type="text/javascript" src="./scripts.a3ea401d0f110c5ce857.js" async></script>
+  </body>
+</html>
