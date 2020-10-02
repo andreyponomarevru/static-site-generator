10b52a92fcc9d8c35f3f40e2662a838de3c0262c
Andrey Ponomarev <info@andreyponomarev.ru>
2020-10-03 01:17:02 +0300

    Rename file

---
+++ build-scripts/githubAPIClient.js
@@ -0,0 +1,79 @@
+const request = require("superagent");
+const { encodeToBase64 } = require("./../utility/encodeToBase64.js");
+
+const USER_AGENT = "Mozilla/5.0";
+const API_BASEURL = "https://api.github.com";
+const API_TOKEN = process.env.GITHUB;
+const REPO_OWNER = "ponomarevandrey";
+
+const credentials = encodeToBase64(`${REPO_OWNER}:${API_TOKEN}`);
+
+function handleRequestError(err) {
+  console.error(err);
+}
+
+// Doc: https://docs.github.com/en/rest/reference/repos#get-a-repository
+async function getRepository(repo) {
+  const endpoint = `${API_BASEURL}/repos/${REPO_OWNER}/${repo}`;
+  console.log(`Getting repository from ${endpoint}...`);
+
+  const res = await request
+    .get(endpoint)
+    .set("user-agent", USER_AGENT)
+    .set("authorization", `Basic ${credentials}`)
+    .on("error", handleRequestError);
+
+  return res.body;
+}
+
+async function getRepoLastCommit(repo) {
+  const endpoint = `${API_BASEURL}/repos/${REPO_OWNER}/${repo}/commits/master`;
+  console.log(`Getting repository last commit from ${endpoint}...`);
+
+  const res = await request
+    .get(endpoint)
+    .set("user-agent", USER_AGENT)
+    .set("authorization", `Basic ${credentials}`)
+    .query({
+      accept: "application/vnd.github.v3+json",
+      owner: REPO_OWNER,
+      repo,
+    })
+    .on("error", handleRequestError);
+
+  const r = {
+    date: res.body.commit.committer.date,
+    message: res.body.commit.message,
+  };
+
+  return r;
+}
+
+async function getFileLastCommit(url) {
+  const repo = url.split("/")[4];
+  const filePath = url.split("blob/master")[1];
+  const endpoint = `${API_BASEURL}/repos/${REPO_OWNER}/${repo}/commits?path=${filePath}&page=1&per_page=1`;
+  console.log(`Getting file's last commit from ${endpoint}...`);
+
+  const res = await request
+    .get(endpoint)
+    .set("user-agent", USER_AGENT)
+    .set("authorization", `Basic ${credentials}`)
+    .query({
+      accept: "application/vnd.github.v3+json",
+      owner: REPO_OWNER,
+      repo,
+    })
+    .on("error", handleRequestError);
+
+  const r = {
+    date: res.body[0].commit.committer.date,
+    message: res.body[0].commit.message,
+  };
+
+  return r;
+}
+
+module.exports.getRepository = getRepository;
+module.exports.getFileLastCommit = getFileLastCommit;
+module.exports.getRepoLastCommit = getRepoLastCommit;
