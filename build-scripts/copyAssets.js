caf20db3ec1e677175ae2b0b5b086e19f4cec068
Andrey Ponomarev <info@andreyponomarev.ru>
2021-01-30 05:47:02 +0300

    Refactor

--- build-scripts/copyAssets.js
+++ build-scripts/copyAssets.js
@@ -3,7 +3,9 @@ const fs = require("fs-extra");
 async function copyAssets(nodes = []) {
   try {
     console.log(`Copying files and folders...`);
-    for (const { from, to } of nodes) await fs.copy(from, to);
+    for (const { from, to } of nodes) {
+      await fs.copy(from, to);
+    }
   } catch (err) {
     console.error(`Error during files and folders copying: ${err.stack}`);
     process.exit(1);
