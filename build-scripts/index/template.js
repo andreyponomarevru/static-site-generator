a45e8083fb087a0d3ce2eb8fcbcfb8404585211a
Andrey Ponomarev <info@andreyponomarev.ru>
2021-01-30 05:47:17 +0300

    Update bio

--- build-scripts/index/template.js
+++ build-scripts/index/template.js
@@ -132,7 +132,7 @@ async function generateHTML({ projects, articles }) {
       <div class="about">
         <div class="content">
           <p class="text text_display_block">
-            !!!!Hi, my name is Andrey, I'm a web developer specializing in the  back-end (Node.js).
+            Hi, my name is Andrey, I'm a web developer specializing in the  back-end (Node.js).
           </p>
           <p class="text text_display_block">
             I wrote my first lines of code back in 2003, and since 2006 I'm doing web development professionally, as an independent contractor. 
