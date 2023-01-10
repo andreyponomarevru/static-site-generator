8116a59f3e5d2667b75165d3fd20373fc849848e
Andrey Ponomarev <info@andreyponomarev.ru>
2023-01-10 13:00:05 +0300

    Change build dir

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ rsync \
   --progress \
   --archive \
   --recursive \
-  ./sssg/build/ "$VPS_USER_SIMPLECLOUD@$VPS_IP_SIMPLECLOUD:/var/www/andreyponomarev.ru/html/"
+  ./build/ "$VPS_USER_SIMPLECLOUD@$VPS_IP_SIMPLECLOUD:/var/www/andreyponomarev.ru/html/"
