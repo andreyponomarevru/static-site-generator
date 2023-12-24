dd4bea255770fe3489bc556004dcd1357171c3c3
Andrey Ponomarev <info@andreyponomarev.ru>
2023-12-24 16:39:01 +0300

    Eco progress status

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rsync \
-  --verbose \
-  --rsh=ssh \
-  --progress \
-  --archive \
-  --recursive \
-  ./build/ "$VPS_USER_SIMPLECLOUD@$VPS_IP_SIMPLECLOUD:/var/www/andreyponomarev.ru/html/"
